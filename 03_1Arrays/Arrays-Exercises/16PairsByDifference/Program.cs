19140524b53583726f9e5bdd5d89c60975da7f8d
VeselinaRaykova <veselinaraikova@gmail.com>
2019-06-11 21:01:23 +0300

    Add Arrays exercise

--- 03_1Arrays/Arrays-Exercises/16PairsByDifference/Program.cs
+++ 03_1Arrays/Arrays-Exercises/16PairsByDifference/Program.cs
@@ -10,7 +10,23 @@ namespace _16PairsByDifference
     {
         static void Main(string[] args)
         {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int diff = int.Parse(Console.ReadLine());
+            int count = 0;
 
+            for (int i = 0; i < numbers.Length - 1; i++)
+            {
+                for (int j = i + 1; j < numbers.Length; j++)
+                {
+                    //Math.Abs(numbers[i] - numbers[j]== diff) seems to be slow -> timeout
+                    if (numbers[i] - numbers[j]== diff || numbers[i] - numbers[j] == -diff)
+                    {
+                        count++;
+                    }
+                }
+            }
+
+            Console.WriteLine(count);
         }
     }
 }
---
+++ 03_1Arrays/Arrays-Exercises/17EqualSums/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _17EqualSums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            int leftSum = 0;
+            int rightSum = 0;
+            int index = -1; //to be out of the array indexes
+
+            for (int num = 0; num < numbers.Length; num++)
+            {
+                for (int leftNum = 0; leftNum < num; leftNum++)
+                {
+                    leftSum += numbers[leftNum];
+                }
+                for (int rightNum = numbers.Length - 1; rightNum > num; rightNum--)
+                {
+                    rightSum += numbers[rightNum];
+                }
+                if (leftSum == rightSum)
+                {
+                    index = num;
+                    break;
+                }
+                leftSum = 0;
+                rightSum = 0;
+            }
+
+            if (index >= 0)
+            {
+                Console.WriteLine(index);
+            }
+            else
+            {
+                Console.WriteLine("no");
+            }
+        }
+    }
+}
