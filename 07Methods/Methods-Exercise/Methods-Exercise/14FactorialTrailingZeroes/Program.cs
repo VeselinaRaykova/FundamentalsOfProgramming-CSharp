c7920716e8ebb26c4a8830996fdd2a165ac4fadc
VeselinaRaykova <veselinaraikova@gmail.com>
2019-07-01 22:32:38 +0300

    Add Methods exercises part 2

--- 07Methods/Methods-Exercise/Methods-Exercise/14FactorialTrailingZeroes/Program.cs
+++ 07Methods/Methods-Exercise/Methods-Exercise/14FactorialTrailingZeroes/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Numerics;
 
 namespace _14FactorialTrailingZeroes
 {
@@ -10,7 +7,38 @@ namespace _14FactorialTrailingZeroes
     {
         static void Main(string[] args)
         {
+            int n = int.Parse(Console.ReadLine());
+            BigInteger factorial = Factorial(n);
+            Console.WriteLine(CountZeroes(factorial));
+        }
 
+        private static int CountZeroes(BigInteger factorial)
+        {
+            int count = 0;
+            while (factorial != 0)
+            {
+                if (factorial % 10 == 0)
+                {
+                    count++;
+                }
+                else
+                {
+                    return count;
+                }
+                factorial /= 10;
+            }
+            return count;
+        }
+
+        private static BigInteger Factorial(int n)
+        {
+            BigInteger factorial = 1;
+            for (int i = 2; i <= n; i++)
+            {
+                factorial *= i;
+            }
+
+            return factorial;
         }
     }
 }
